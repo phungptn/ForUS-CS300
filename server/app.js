84669dcae170c9483961064de1eac1ff410061d5
Minh <minhvip08@gmail.com>
2023-11-16 15:37:47 +0700

    basic api

--- server/app.js
+++ server/app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-// const tourRouter = require('./routes/tourRoutes');
-// const userRouter = require('./routes/userRoutes');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const morgan = require('morgan');
-
+const route = require('./routes/userRoute');
 
 // // Middleware
 // if (process.env.NODE_ENV === 'development') {
@@ -13,23 +13,9 @@ const morgan = require('morgan');
 app.use(morgan('dev'));
 
 app.use(express.json());
-// app.use(express.static(`${__dirname}/public`));
+app.use(cookieParser());
 
-// app.use((req, res, next) => {
-//   req.requestTime = new Date().toISOString();
-//   next();
-// });
+route(app);
 
-// app.get('/', (req, res) => {
-//   res.status(400).json({ message: 'Hello from Express!', app: 'Natrous' });
-// });
-
-// app.listen(port, () => {
-//   console.log(`Example app listening at http://localhost:${port}`);
-// });.
-
-// Routes
-// app.use('/api/v1/tours', tourRouter);
-// app.use('/api/v1/users', userRouter);
 
 module.exports = app;
