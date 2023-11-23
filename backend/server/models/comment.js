11e61a4445342085e6bacbcace82718a16f96896
trietng <93473943+trietng@users.noreply.github.com>
2023-11-23 21:49:29 +0700

    update schema

--- backend/server/models/comment.js
+++ backend/server/models/comment.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const CommentSchema = new Schema({
-    title: { type: String, required: true, maxLength: 128, minLength: 1},
     body: { type: String, required: true, maxLength: 4096, minLength: 1},
     upvoted: [{ type: Schema.Types.ObjectId, ref: 'User'}],
     downvoted: [{ type: Schema.Types.ObjectId, ref: 'User'}],
