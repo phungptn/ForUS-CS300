4f43923c6b9c7155e49fc0a785d7a15a987b086f
Haruna <59250517+Haruna38@users.noreply.github.com>
2023-12-04 21:35:24 +0900

    token.new

--- backend/config/jwtToken.js
+++ backend/config/jwtToken.js
@@ -1,20 +1,16 @@
 // generating JWT
-import jwt from "jsonwebtoken";
+const jwt = require("jsonwebtoken");
 
+const DEFAULT_DAY_ALIVE = 10 * 1000;//12 * 30 * 24 * 60 * 60 * 1000;
 const JWT_SECRET = process.env.JWT_SECRET || "secret";
 
-const generateToken = (id, dayLive) => {
-  return jwt.sign({ id }, JWT_SECRET, { expiresIn: dayLive });
+const generateToken = (user) => {
+  return jwt.sign({ id: user.id, session: user.sessionStart }, JWT_SECRET, { noTimestamp: true });
 };
 
 const decodeToken = (accessToken) => {
-  return jwt.verify(accessToken, JWT_SECRET, (err, decoded) => {
-    if (err) {
-      return err;
-    } else {
-      return decoded;
-    }
-  });
+  try { return jwt.verify(accessToken, JWT_SECRET); }
+  catch (e) { return null }
 };
 
-export { generateToken, decodeToken };
+module.exports = { generateToken, decodeToken, DEFAULT_DAY_ALIVE };
