ccdc3d53ee6ebd35477656b587cf7d62fbeef4db
trietng <93473943+trietng@users.noreply.github.com>
2023-12-15 00:08:51 +0700

    Add create group

--- backend/server/routers/group.js
+++ backend/server/routers/group.js
@@ -1,10 +1,11 @@
 const express = require('express');
-import { readGroup, updateGroup, deleteGroup } from '../controllers/group';
+import { createGroup, readGroup, updateGroup, deleteGroup } from '../controllers/group';
 import { createBox } from '../controllers/box';
 import { isAdmin } from '../controllers/user';
 const router = express.Router();
 
 router.get('/', readGroup);
+router.post('/', isAdmin, createGroup);
 router.post('/:group_id/box', isAdmin, createBox);
 router.put('/:group_id', isAdmin, updateGroup);
 router.delete('/:group_id', isAdmin, deleteGroup);
