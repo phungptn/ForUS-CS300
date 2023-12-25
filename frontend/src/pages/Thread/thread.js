62b0ea715dc156fee55dfd8be17d5ab140a9c32e
Hứa Trường Khả <htkha1203@gmail.com>
2023-12-25 18:35:59 +0700

    Link to thread

---
+++ frontend/src/pages/Thread/thread.js
@@ -0,0 +1,17 @@
+// import { useParams, useNavigate, useLocation } from "react-router-dom";
+// import {  useEffect, useState } from "react";
+// import { instance } from "../../api/config";
+// import { BoxDescription, CreateThreadButton, Pagination } from "./UserControl/usercontrol";
+// import CommentCard from "./CommentCard/commentcard";
+// import { BoxContext } from "./context";
+// import { checkModerator } from "../../utils/checkModerator";
+// import { BoxControl } from "./BoxManagement/boxmanagement";
+
+export default function Thread() {
+    return (
+        <>
+            
+        </>
+    );
+}
+
