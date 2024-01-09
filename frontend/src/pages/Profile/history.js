0df7af592d7e5f8d108d7cb7f335f2b0e1d5ca24
phungptn <ngothaiphung412@gmail.com>
2024-01-09 18:24:22 +0700

    Update history.js
    add fake pagination

--- frontend/src/pages/Profile/history.js
+++ frontend/src/pages/Profile/history.js
@@ -4,6 +4,7 @@ import React, { useState, useEffect } from "react";
 import ThreadCard from "../Box/ThreadCard/threadcard"; // Update with the correct path
 import { getThreadHistory } from "../../api/user"; // Update with the correct API function
 import { BoxContext } from "../Box/context";
+import { Pagination } from "../Box/UserControl/usercontrol";
 
 const ThreadHistory = ({ user_id, changeID }) => {
   const location = useLocation();
@@ -33,10 +34,34 @@ const ThreadHistory = ({ user_id, changeID }) => {
     <div>
       <h1 className="mb-3 text-white">Thread History</h1>
       {threadHistory.map((thread) => (
-        <BoxContext.Provider value={{ box: null, setBox: () => {}, moderatorStatus: false, setAutoRedirect: () => {} }}>
-          <ThreadCard thread={thread} search={false} hideSection={true}/>
+        <BoxContext.Provider
+          value={{
+            box: null,
+            setBox: () => {},
+            moderatorStatus: false,
+            setAutoRedirect: () => {},
+          }}
+        >
+          <ThreadCard thread={thread} search={false} hideSection={true} />
         </BoxContext.Provider>
       ))}
+      <nav>
+        <ul className="pagination justify-content-center">
+          <li className="page-item">
+            <button className="page-link" aria-label="Previous" disabled>
+              <span aria-hidden="true">&laquo;</span>
+            </button>
+          </li>
+          <li className="page-item active">
+            <button className="page-link">1</button>
+          </li>
+          <li className="page-item">
+            <button className="page-link" aria-label="Next" disabled>
+              <span aria-hidden="true">&raquo;</span>
+            </button>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 };
